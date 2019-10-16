352109f09e4e34286e379b279e3b76a5523c543a
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2019-10-16 10:18:52 +0800

    init project

---
+++ mybatis_plus/src/test/java/com/atguigu/MybatisPlusApplicationTests.java
@@ -0,0 +1,47 @@
+package com.atguigu;
+
+import com.atguigu.entity.User;
+import com.atguigu.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPlusApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectList() {
+        System.out.println(("----- selectAll method test ------"));
+        //UserMapper 中的 selectList() 方法的参数为 MP 内置的条件封装器 Wrapper
+        //所以不填写就是无任何条件
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsert(){
+
+        Date date =new Date(1184279624474025986L);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd Hh:mm:ss").format(date));
+
+        User user = new User();
+        user.setName("Helen");
+        user.setAge(18);
+        user.setEmail("55317332@qq.com");
+
+        int result = userMapper.insert(user);
+        System.out.println(result); //影响的行数
+        System.out.println(user); //id自动回填
+    }
+
+}
